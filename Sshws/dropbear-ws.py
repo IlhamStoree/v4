3d0f032ce8ea6a035d74811d935c729035604cbd
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-07-27 16:22:10 +0700

    Update dropbear-ws.py

--- Sshws/dropbear-ws.py
+++ Sshws/dropbear-ws.py
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:109'
-RESPONSE = 'HTTP/1.1 101 <font color="red"><b><i>Bringas-family</font></b></i>\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <font color="yellow"><b><i>ILHAM-STORE</font></b></i>\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):

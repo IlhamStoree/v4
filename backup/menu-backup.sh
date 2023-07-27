96b5659404dc9bfeda7af852d8a8c03b58ed7f26
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-07-27 19:54:09 +0700

    Update menu-backup.sh

--- backup/menu-backup.sh
+++ backup/menu-backup.sh
@@ -18,7 +18,7 @@ echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
 echo -e
 echo -e "${BlueCyan}              Menu Backup                   "
 echo -e
-echo -e "             Bringas-family"
+echo -e "             Ilham-Store"
 echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
 echo
 echo
@@ -41,7 +41,7 @@ figlet "backup" | lolcat
 backup
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script bringas"
+echo "Terimakasih sudah menggunakan layanan script ilham-store"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -52,7 +52,7 @@ figlet "Restore" | lolcat
 restore
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script Bringas"
+echo "Terimakasih sudah menggunakan layanan script ilham-store"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -62,7 +62,7 @@ figlet "strt"
 strt
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script Bringas"
+echo "Terimakasih sudah menggunakan layanan script ilham-store"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -71,7 +71,7 @@ then
 limitspeed
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script Bringas"
+echo "Terimakasih sudah menggunakan layanan script ilham-store"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -81,7 +81,7 @@ then
 autobackup
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script Bringas"
+echo "Terimakasih sudah menggunakan layanan script ilham-store"
 echo
 echo "--------------------------------------------------------"
 fi
